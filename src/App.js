80a100ff88dff5203c63c89d0cc82221b93a8729
JackMcAulay <47449092+JackMcAulay@users.noreply.github.com>
2023-10-26 21:09:09 +0100

    added shake, removed unessary orbs

--- src/App.js
+++ src/App.js
@@ -16,12 +16,13 @@ const Magic8Ball = () => {
     "The stars\nsay yes",
     "It is\ncertain"
   ];
-
   const [answer, setAnswer] = useState("");
   const [isVisible, setIsVisible] = useState(false);
+  const [isShaking, setIsShaking] = useState(false);
 
   const handleAskQuestion = () => {
     setIsVisible(false);
+    setIsShaking(true);
     setTimeout(() => {
       const randomIndex = Math.floor(Math.random() * answers.length);
       const randomAnswer = answers[randomIndex].split("\n").map((line, index) => (
@@ -29,15 +30,14 @@ const Magic8Ball = () => {
       ));
       setAnswer(randomAnswer);
       setIsVisible(true);
+      setIsShaking(false);
     }, 500);
   };
 
   return (
     <div className="container">
-      <div className={`orb`}>
-      <div className={`orb2`}></div>
-      <div className={`orb3`}></div>
-      <div className={`orb4`}></div>
+      <div className={`orb ${isShaking ? "shake" : ""}`}>
+        <div className={`orb2 ${isShaking ? "shake" : ""}`}></div>
           <div className={`triangle ${isVisible ? "visible" : ""}`}></div>
           <div className={`text ${isVisible ? "visible" : ""}`} style={{ fontSize: "0.75rem" }}>
           {answer}
